137943f99bf922f2661791e619d2ac26ee812c39
Shawn Sandy <shawnsandy04@gmail.com>
2013-08-01 22:12:43 -0400

    Readme updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 pico_tpl
 ========
 
-Pico Templates (pico_tpl)
+A simple templating(tpl) system for pico that allows you to include html templates in your pico theme(s).
--- pico_tpl.php
+++ pico_tpl.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * A simple templating system for pico that allows you to include html templates in your pico theme(s)
+ * A simple templating(tpl) system for pico that allows you to include html templates in your pico theme(s)
  * @package Pico
  * @subpackage Pico Tpl
  * @since BJ 1.0 TODO
