166fee7d0d2a996e18cf7914c1623d5069d6bb0c
Shawn Sandy <shawnsandy04@gmail.com>
2013-08-04 12:05:01 -0400

    Added the ability to load reuseable html twig views to themes

--- pico_tpl.php
+++ pico_tpl.php
@@ -7,12 +7,11 @@
  * @since BJ 1.0 TODO
  * @author Shawn Sandy <shawnsandy04@gmail.com>
  */
-
 class Pico_Tpl {
 
     private $tpl_name,
             $theme = 'default',
-            $tpl_array = array('header','content','footer','cover','sidebar');
+            $tpl_array = array('header', 'content', 'footer', 'cover', 'sidebar');
 
     public function __construct() {
 
@@ -29,33 +28,75 @@ class Pico_Tpl {
 
     public function config_loaded(&$settings) {
 
-        if(isset($settings['tpl_array']))
+        if (isset($settings['tpl_array']))
             $this->tpl_array = $settings['tpl_array'];
 
-        if(isset($settings['theme']))
-            $this->theme =  $settings['theme'];
-
+        if (isset($settings['theme']))
+            $this->theme = THEMES_DIR . $settings['theme'];
     }
-
 
     public function file_meta(&$meta) {
 
         $config = $meta;
-        if(isset($config['slug']))
-            $this->tpl_name = strtolower ($config['tpl']);
-
+        if (isset($config['slug']))
+            $this->tpl_name = strtolower($config['tpl']);
     }
 
     public function before_render(&$twig_vars, &$twig) {
 
-        foreach ($this->tpl_array as $value) {
-            $tpl[$value] = 'tpl/'.$value.'.html';
-            if(file_exists(THEMES_DIR.$this->theme.'/tpl/'.$this->tpl_name.'-'.$value.'.html'))
-            $tpl[$value] = 'tpl/'.$this->tpl_name.'-'.$value.'.html';
-        }
-       // var_dump($page_tpl);
-       $twig_vars['tpl'] = $tpl ;
+        // var_dump($page_tpl);
+        $twig_vars['tpl'] = $this->get_tpl();
 
+        $views = $this->get_views();
+        $twig_vars['views'] = $views;
+        //var_dump($views);
+
+    }
+
+    private function get_tpl() {
+
+        foreach ($this->tpl_array as $value) {
+            $tpl[$value] = 'tpl/' . $value . '.html';
+            if (file_exists($this->theme . '/tpl/' . $this->tpl_name . '-' . $value . '.html'))
+                $tpl[$value] = 'tpl/' . $this->tpl_name . '-' . $value . '.html';
+        }
+        return $tpl;
+    }
+
+    private function get_views() {
+
+        $view_dir = $this->theme . '/tpl/views';
+        $views = $this->get_files($view_dir);
+
+        foreach ($views as $key) {
+            $view[$key] = 'tpl/views/'. $key.'.html';
+        }
+        //var_dump($views);
+        if(!isset($view))
+            return array(0);
+
+        return $view;
+
+    }
+
+    // pico.php lib
+    private function get_files($directory, $ext = '.html') {
+        if (!is_dir($directory))
+            return false;
+
+        $array_items = array();
+        if ($handle = opendir($directory)) {
+            while (false !== ($file = readdir($handle))) {
+
+                $file = $directory . "/" . $file;
+                if (!$ext || strstr($file, $ext))
+                //$array_items[] = preg_replace("/\/\//si", "/", $file);
+
+                    $array_items[] = basename($file, $ext);
+            }
+            closedir($handle);
+        }
+        return $array_items;
     }
 
 }
